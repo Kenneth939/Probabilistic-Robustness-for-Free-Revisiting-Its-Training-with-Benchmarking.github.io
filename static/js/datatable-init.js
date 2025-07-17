e991f381b81a9cf8cab14d70f14bc060b812dbd8
Kenneth1129 <157056087+Kenneth939@users.noreply.github.com>
2025-07-17 17:21:45 +0100

    Update datatable-init.js

--- static/js/datatable-init.js
+++ static/js/datatable-init.js
@@ -1,5 +1,7 @@
+// static/js/datatable-init.js
+
 $(document).ready(function() {
-  const table = $('#leaderboard-table').DataTable({
+  $('#leaderboard-table').DataTable({
     ajax: {
       url: 'static/src/data/prbench_table8.json',
       dataSrc: ''
@@ -9,26 +11,25 @@ $(document).ready(function() {
       { data: 'model' },
       { data: 'method' },
       { data: 'acc' },
-      { data: 'pr_uniform.0.03' },
-      { data: 'pr_uniform.0.08' },
-      { data: 'pr_uniform.0.10' },
-      { data: 'pr_uniform.0.12' },
-      { data: 'pr_gaussian.0.03' },
-      { data: 'pr_gaussian.0.08' },
-      { data: 'pr_gaussian.0.10' },
-      { data: 'pr_gaussian.0.12' },
-      { data: 'pr_laplace.0.03' },
-      { data: 'pr_laplace.0.08' },
-      { data: 'pr_laplace.0.10' },
-      { data: 'pr_laplace.0.12' },
-      { data: 'ge.uni' },
-      { data: 'ge.gau' },
-      { data: 'ge.lap' }
+      { data: 'pr_uniform', render: data => data['0.03'] },
+      { data: 'pr_uniform', render: data => data['0.08'] },
+      { data: 'pr_uniform', render: data => data['0.10'] },
+      { data: 'pr_uniform', render: data => data['0.12'] },
+      { data: 'pr_gaussian', render: data => data['0.03'] },
+      { data: 'pr_gaussian', render: data => data['0.08'] },
+      { data: 'pr_gaussian', render: data => data['0.10'] },
+      { data: 'pr_gaussian', render: data => data['0.12'] },
+      { data: 'pr_laplace', render: data => data['0.03'] },
+      { data: 'pr_laplace', render: data => data['0.08'] },
+      { data: 'pr_laplace', render: data => data['0.10'] },
+      { data: 'pr_laplace', render: data => data['0.12'] },
+      { data: 'ge', render: data => data.uni },
+      { data: 'ge', render: data => data.gau },
+      { data: 'ge', render: data => data.lap }
     ],
     pageLength: 25,
-    order: [[ 3, 'desc' ]],  // 按 Acc 排序
+    order: [], // 保持输入顺序，不做初始排序
     initComplete: function() {
-      // 在 Dataset/Model/Method 三列生成下拉筛选
       this.api().columns([0,1,2]).every(function() {
         const column = this;
         const select = $('<select class="form-select"><option value="">All</option></select>')
@@ -43,3 +44,4 @@ $(document).ready(function() {
     }
   });
 });
+
